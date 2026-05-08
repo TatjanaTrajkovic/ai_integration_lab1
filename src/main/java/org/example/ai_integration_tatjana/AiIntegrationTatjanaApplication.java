2e27b9f7573f77ee1441606d5ae4be908c235a8a
Tatjana <malina91983@gmail.com>
2026-05-08 10:17:24 +0200

    First commit

---
+++ src/main/java/org/example/ai_integration_tatjana/AiIntegrationTatjanaApplication.java
@@ -0,0 +1,13 @@
+package org.example.ai_integration_tatjana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiIntegrationTatjanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiIntegrationTatjanaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ai_integration_tatjana/AiIntegrationTatjanaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ai_integration_tatjana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiIntegrationTatjanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
